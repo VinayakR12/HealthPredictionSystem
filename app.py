65fd5a050f8780b3accb51ff5faa3c0b3eb8e201
Vinayak Rhatankar <157618927+VinayakR12@users.noreply.github.com>
2025-03-02 19:07:40 +0530

    Update app.py

--- app.py
+++ app.py
@@ -73,4 +73,5 @@ def parkinsons():
 #     app.run(debug=True)
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    # Required for Render Deployment
+    app.run(host="0.0.0.0", port=10000, debug=True)
